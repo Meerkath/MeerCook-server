01d13e03fc40f31ebb8eb227743d669fdd8c5f21
Alexandre Viale <tutunelel@gmail.com>
2022-02-18 16:03:25 +0100

    --

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import recipes from './routes/recipes.js'
 const app = express()
 
-app.use('MeerCook-server/recipes', recipes)
-app.get('/', function (req, res) {
+app.use('/MeerCook-server/recipes', recipes)
+app.get('/MeerCook-server', function (req, res) {
   res.send("hello")
 })
 app.listen()
