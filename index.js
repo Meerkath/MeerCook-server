d700919059b3bfcf64bde4ac45579cf3249ac54c
Alexandre Viale <tutunelel@gmail.com>
2022-02-18 17:40:21 +0100

    --

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 // import recipes from './routes/recipes.js'
 const app = express()
 
-app.use('/meercook-server/recipes', recipes)
+// app.use('/meercook-server/recipes', recipes)
 app.get('/meercook-server', function (req, res) {
   console.log('hello');
   res.send("hello")
