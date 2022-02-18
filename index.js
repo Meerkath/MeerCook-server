43f405ee0e7b0b7f7bebdc5f1c6e27a2e95375b3
Alexandre Viale <tutunelel@gmail.com>
2022-02-18 17:40:08 +0100

    removed importing

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-import express from 'express'
-import recipes from './routes/recipes.js'
+const express = require('express')
+// import recipes from './routes/recipes.js'
 const app = express()
 
 app.use('/meercook-server/recipes', recipes)
 app.get('/meercook-server', function (req, res) {
+  console.log('hello');
   res.send("hello")
 })
 app.listen()
