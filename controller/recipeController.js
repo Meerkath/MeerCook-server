389742ca2ac4aeab8f7ef7460f0a709dc2f813c0
Alexandre Viale <tutunelel@gmail.com>
2022-04-15 14:13:10 +0200

    now possible to get image

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -1,6 +1,26 @@
+const fs = require('fs')
+const path = require('path')
 const {getRecipesByUserId} = require('../model/recipeModel.js')
+
 module.exports = {
   getRecipes: async (_req, res) => {
     res.send(await getRecipesByUserId(res.locals.user.id))
+  },
+  getImage: async (req, res) => {
+    if (!req.params.imgId) {
+      res.status(400)
+        .send('Please provide imgId attribute.'); return
+    }
+    // eslint-disable-next-line max-len
+    const imgPath = path.join(__dirname, '../images', `${res.locals.user.id}_${req.params.imgId}.jpg`)
+    console.log(imgPath)
+    try {
+      if (!fs.existsSync(imgPath)) {
+        res.status(404).send('Image not found.'); return
+      }
+      res.sendFile(imgPath)
+    } catch (err) {
+      throw new Error('Error while getting image.', err)
+    }
   },
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const {authenticateToken} = require('../controller/auth.js')
 const router = new express.Router()
-const {getRecipes} = require('../controller/recipeController.js')
+const {getRecipes, getImage} = require('../controller/recipeController.js')
 
 router.get('/', authenticateToken, getRecipes)
+router.get('/image/:imgId', authenticateToken, getImage)
 
 module.exports = router
